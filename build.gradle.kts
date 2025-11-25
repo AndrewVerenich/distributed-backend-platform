6c7a77db12a13c7098d94c0752145bc277b869eb
andrey.verenich <andrey.verenich@moneyman.ru>
2025-11-25 10:30:40 +0300

    init gradle

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  kotlin("jvm") version "1.9.20" apply false
+  kotlin("plugin.spring") version "1.9.20" apply false
+  id("org.springframework.boot") version "3.1.5" apply false
+  id("io.spring.dependency-management") version "1.1.4" apply false
+}
+
+allprojects {
+  group = "com.andver"
+  version = "1.0.0"
+
+  repositories {
+    mavenCentral()
+  }
+}
+
